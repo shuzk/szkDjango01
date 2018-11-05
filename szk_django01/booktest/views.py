a087df8aed5add56fc2b20a42eec96780ba70880
szk_django01 <szk_django01@163.com>
2018-11-05 20:23:44 +0800

    项目完成——定义视图

--- szk_django01/booktest/views.py
+++ szk_django01/booktest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, RequestContext
+from booktest.models import BookInfo
 
 
 # Create your views here.
@@ -18,6 +19,22 @@ from django.template import loader, RequestContext
 #     return HttpResponse(template.render(context))
 
 
+# def index(request):
+#     context = {"title": "图书列表", "list": range(10)}
+#     return render(request, "booktest/index.html", context)
+
+
 def index(request):
-    context = {"title": "图书列表", "list": range(10)}
-    return render(request, "booktest/index.html", context)
+    # 查询所有图书
+    booklist = BookInfo.objects.all()
+    # 将图书列表传递到模板中，然后渲染模板
+    return render(request, "booktest/index.html", {"booklist": booklist})
+
+
+def detail(request, bid):
+    # 根据图书编号对应图书
+    book = BookInfo.objects.get(id=int(bid))
+    # 查找book图书中的所有英雄信息
+    heros = book.heroinfo_set.all()
+    # 将图书信息传递到模板中，然后渲染模板
+    return render(request, "booktest/detail.html", {"book": book, "heros": heros})
