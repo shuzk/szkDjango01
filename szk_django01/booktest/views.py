d06f7b2851e3d497c971f01550c7c7ff6adf02e5
szk_django01 <szk_django01@163.com>
2018-11-05 20:16:52 +0800

    模板——视图调用模板简写

--- szk_django01/booktest/views.py
+++ szk_django01/booktest/views.py
@@ -9,10 +9,15 @@ from django.template import loader, RequestContext
 #     return HttpResponse("index")
 
 
+# def index(request):
+#     # 1.获取模板
+#     template = loader.get_template("booktest/index.html")
+#     # 2.定义上下文
+#     context = RequestContext(request, {"title": "图书列表", "list": range(10)})
+#     # 3.渲染模板
+#     return HttpResponse(template.render(context))
+
+
 def index(request):
-    # 1.获取模板
-    template = loader.get_template("booktest/index.html")
-    # 2.定义上下文
-    context = RequestContext(request, {"title": "图书列表", "list": range(10)})
-    # 3.渲染模板
-    return HttpResponse(template.render(context))
+    context = {"title": "图书列表", "list": range(10)}
+    return render(request, "booktest/index.html", context)
