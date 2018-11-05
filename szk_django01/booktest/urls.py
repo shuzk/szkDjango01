277253bff773718bb6350790d03e4245f8faa544
szk_django01 <szk_django01@163.com>
2018-11-05 20:29:04 +0800

    项目完成——定义URLconf

--- szk_django01/booktest/urls.py
+++ szk_django01/booktest/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from booktest import views
 
+# 引入视图模块
 urlpatterns = [
+    # 配置首页url
     url(r"^$", views.index),
+    # 配置详细页url， \d+表示多个数字，小括号用于取值
+    url(r"^(\d+)/$", views.detail)
 ]
