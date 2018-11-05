710f29f2281039489c14a2a12e1a650bccee964c
szk_django01 <szk_django01@163.com>
2018-11-05 20:13:28 +0800

    模板——视图调用模板

--- szk_django01/booktest/views.py
+++ szk_django01/booktest/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, RequestContext
 
 
 # Create your views here.
+#
+# def index(request):
+#     return HttpResponse("index")
+
 
 def index(request):
-    return HttpResponse("index")
+    # 1.获取模板
+    template = loader.get_template("booktest/index.html")
+    # 2.定义上下文
+    context = RequestContext(request, {"title": "图书列表", "list": range(10)})
+    # 3.渲染模板
+    return HttpResponse(template.render(context))
